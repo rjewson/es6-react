84895355412180d3b2446eacbf1ea79d60c90eed
Richard Jewson <richard.jewson@AMSNOTE005.local>
2016-09-02 14:02:04 +0200

    setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './main.js',
+    ouput: {
+        path: './',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    }
+}
