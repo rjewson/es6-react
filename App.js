41d91d51e17cb9c4dab132c1dc1640bdecf78b55
Richard Jewson <richard.jewson@AMSNOTE005.local>
2016-09-02 14:40:43 +0200

    stuff

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     render() {
-        return <div>Hello there u!</div>
+        let name = this.props.name;
+        return (
+            <div>Hello there {name}!!</div>
+        )
     }
 }
 
-export default App;
+App.propTypes = {
+    name: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+ReactDOM.render(
+    <App name="Richard"/>,
+    document.getElementById('app')
+);
+// export default App;
